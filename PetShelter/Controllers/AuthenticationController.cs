4dd1db01692530462375b3d89bf486be731aee52
HassanAsh34 <126121195+HassanAsh34@users.noreply.github.com>
2025-04-28 21:16:16 +0300

    fixed

--- PetShelter/Controllers/AuthenticationController.cs
+++ PetShelter/Controllers/AuthenticationController.cs
@@ -1,8 +1,8 @@
-﻿using System.Text;
+﻿using System.Security.Claims;
+using System.Text;
 using System.Text.Json;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using PetShelter.Data;
 using PetShelter.DTOs;
 using PetShelter.MiddleWare;
@@ -11,20 +11,17 @@ using PetShelter.Services;
 
 namespace PetShelter.Controllers
 {
-    [Route("AuthApi")]
+	[Route("AuthApi")]
 	[ApiController]
 	public class AuthenticationController : ControllerBase
 	{
-		//private readonly UserRepository _userRepository;
-
 		private readonly UserService _userService;
-
 		private readonly JWT _token;
-		public AuthenticationController(UserService userService, JWT token)//, UserRepository userRepository) //dont forget the constructor injection 
+
+		public AuthenticationController(UserService userService, JWT token)
 		{
 			_userService = userService ?? throw new ArgumentNullException(nameof(userService));
 			_token = token ?? throw new ArgumentNullException(nameof(token));
-			//_userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
 		}
 
 		[HttpPost("Register")]
@@ -35,18 +32,18 @@ namespace PetShelter.Controllers
 			var res = await _userService.Register(user);
 			if (res == null)
 			{
-				return BadRequest(new { message = "user already exists" });
+				return BadRequest(new { message = "User already exists" });
 			}
 			else
 			{
 				return StatusCode(StatusCodes.Status201Created, new
 				{
 					res = res,
-					meessage = "User Created Successfully"
+					message = "User Created Successfully"
 				});
 			}
 		}
-		//[Authorize]
+
 		[HttpPost("Login")]
 		[ProducesResponseType(StatusCodes.Status200OK)]
 		[ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -61,53 +58,50 @@ namespace PetShelter.Controllers
 						return BadRequest(new { message = "Your account is currently inactive" });
 					case 1:
 						var token = _token.GenerateToken(res);
-						return Ok(new { Token = token, User = res });
+						return Ok(new { token = token, user = res }); // lowercase keys (token, user) to match frontend
 					case 2:
 						return BadRequest(new { message = "Your account has been suspended" });
 					default:
-						//return StatusCodes.Status500InternalServerError;
 						return BadRequest("Something went wrong");
 				}
 			}
 			else
 			{
-				return BadRequest(new { Message = "Invalid Email or Password" });
+				return BadRequest(new { message = "Invalid Email or Password" });
 			}
 		}
 
 		[Authorize]
-		[HttpGet()]
+		[HttpGet]
 		public ActionResult<UserDto> Protected()
 		{
-			var userDetailsJson = User.FindFirst("userDetails")?.Value;
-			var userDetails = JsonSerializer.Deserialize<UserDto>(userDetailsJson);
+			var currentUser = GetCurrentUser();
+			if (currentUser == null)
+			{
+				return Unauthorized(new { message = "User details not found in token" });
+			}
 
-			return Ok(userDetails);
-
-			
+			return Ok(currentUser);
 		}
 
+		// ➡️ NEW Helper Method for User extraction
+		private UserDto GetCurrentUser()
+		{
+			var userDetailsJson = User.FindFirst(ClaimTypes.UserData)?.Value;
+			if (string.IsNullOrEmpty(userDetailsJson))
+			{
+				return null;
+			}
 
-
-		//[HttpGet("/GetKey")]
-		//public ActionResult<JWT> generateToken(JWT j)
-		//{
-		//	return Ok(j);
-		//}
-		//		public async Task<ActionResult<User>> Register([FromBody] User user) bec that one will require a custom deserializer
-		//		{
-		//			if (await _userRepository.UserExistence(user.Email) == true)
-		//			{
-		//				return BadRequest(new { message = "user already exists" });
-		//			}
-		//			else
-		//{
-		//	return Ok(new
-		//	{
-		//		User = await _userRepo2sitory.AddUser(user),
-		//		Message = "User was added successfully"
-		//	});
-		//}
-		//		}
+			try
+			{
+				return JsonSerializer.Deserialize<UserDto>(userDetailsJson);
+			}
+			catch (JsonException)
+			{
+				return null;
+			}
+		}
 	}
 }
+	
